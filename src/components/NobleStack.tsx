1e4c9230a14a38149a2ee7b629387e9032d93954
Danielle Man <daniman0808@gmail.com>
2020-06-05 18:36:42 -0700

    add margin around nobles

--- src/components/NobleStack.tsx
+++ src/components/NobleStack.tsx
@@ -3,13 +3,13 @@ import { NobleCard } from './NobleCard';
 import * as Types from '../types';
 
 export const NobleStack: React.FC<{
-  nobles: (Types.GameBoard_game_players_reservedCards)[];
-}> = ( { nobles } ) => (
-  <div style={{ position: 'relative', marginBottom: 10 }}>
+  nobles: Types.GameBoard_game_players_reservedCards[];
+}> = ({ nobles }) => (
+  <div style={{ position: 'relative', marginBottom: 40, marginTop: 40 }}>
     <div style={{ display: 'flex' }}>
       {nobles.map((card) => (
         <NobleCard key={card?.id} card={card} />
       ))}
     </div>
   </div>
-)
+);
