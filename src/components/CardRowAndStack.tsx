23db770b45189423e821883bd02dc9e0088c2275
Chang Wang <garnwraly@gmail.com>
2020-10-01 00:31:23 -0400

    animate cards out

--- src/components/CardRowAndStack.tsx
+++ src/components/CardRowAndStack.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, PlaceholderCard } from './Card';
 import * as Types from '../types';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export type TopOfDeck = { type: Types.CardStackType };
 
@@ -12,20 +13,29 @@ export const CardRowAndStack: React.FC<{
   onSelect?: (c: Types.CardSelection | TopOfDeck) => void;
 }> = ({ cards, turnCardState, level, remaining, onSelect }) => (
   <div style={{ display: 'flex', marginBottom: 10 }}>
-    {cards.map((card, i) =>
-      turnCardState && (turnCardState as Types.CardSelection).id === card.id ? (
-        <PlaceholderCard label="x" key={card.id} />
-      ) : (
-        <Card
+    <AnimatePresence>
+      {cards.map((card, i) => (
+        <motion.div
           key={card.id}
-          card={card}
-          onSelect={(card: Types.CardSelection) => {
-            if (onSelect) onSelect(card);
-          }}
-          availableForPurchase={true}
-        />
-      )
-    )}
+          initial={false}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          {turnCardState &&
+          (turnCardState as Types.CardSelection).id === card.id ? (
+            <PlaceholderCard label="x" />
+          ) : (
+            <Card
+              card={card}
+              onSelect={(card: Types.CardSelection) => {
+                if (onSelect) onSelect(card);
+              }}
+              availableForPurchase={true}
+            />
+          )}
+        </motion.div>
+      ))}
+    </AnimatePresence>
     <div
       className="clickable secret card"
       style={{
